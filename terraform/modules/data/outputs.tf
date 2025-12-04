c7b1d8b4f47b46b6886183a5925b0503c29124df
aharon <aharonysharf@gmail.com>
2025-12-04 14:58:50 +0200

    fix:

--- terraform/modules/data/outputs.tf
+++ terraform/modules/data/outputs.tf
@@ -27,12 +27,12 @@ output "rds_master_secret_arn" {
 
 output "rds_username" {
   description = "RDS PostgreSQL user name"
-  value = aws_db_instance.postgres.username
+  value       = aws_db_instance.postgres.username
 }
 
 output "rds_db_name" {
   description = "RDS PostgreSQL DB name"
-  value = aws_db_instance.postgres.db_name
+  value       = aws_db_instance.postgres.db_name
 }
 
 # Redis has been moved to Asterisk EC2 server - see compute module outputs
